0feada923c2eeb12ae2a1585fa139e4db3d514ce
MashaShamash <mariapanasenko26@yandex.ru>
2024-05-24 15:41:51 +0300

    catNotSay

--- sudoku.js
+++ sudoku.js
@@ -1,10 +1,23 @@
 const fs = require("fs");
-function read() {
-  
-  return fs.readFileSync("./puzzles.txt", "utf-8");
-  
+function read(num) {
+  const fs = require("fs");
+  const read1 = fs.readFileSync("./puzzles.txt", "utf-8").split("\r\n");
+  const getSudoku = read1[num - 1].split("");
+  const board = [];
+  for (let i = 0; i < 9; i++) {
+    board.push(getSudoku.slice(i * 9, (i + 1) * 9));
+  }
+  const number = board.map((row) =>
+    row.map((el) => {
+      if (el === "-") {
+        return (el = null);
+      }
+      return Number(el);
+    })
+  );
+  return number;
 }
-read()
+console.table(read(1));
 
 function solve() {
   /**
